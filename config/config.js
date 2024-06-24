68f5640d5f93abbffc7361e0ee3da94154a71def
PexArt-web <pexart74@gmail.com>
2024-06-24 20:06:37 +0100

    update

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDoL0BI-a7Y5TiHzwaWbjwgBKahpV7azpU",
+  authDomain: "px-luxury-289ba.firebaseapp.com",
+  projectId: "px-luxury-289ba",
+  storageBucket: "px-luxury-289ba.appspot.com",
+  messagingSenderId: "1091968351878",
+  appId: "1:1091968351878:web:dbb11496b81afa7d7edb07",
+};
+
+// paystack
+
+const unlock = "pk_test_bb3514eaf08a35c25e2d8d0a8c887d391362661d"
+const blueEmail = 'AfYrDxksH0WRVUqL'
+export{firebaseConfig , unlock, blueEmail}
